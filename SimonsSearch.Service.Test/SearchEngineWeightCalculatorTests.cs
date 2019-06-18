82a53ad1e849a5f3a384ae1cea0e4670bb07e4eb
Nemanja Babin <babinnnemanja@gmail.com>
2019-06-18 19:35:12 +0200

    Added missing unit tests

--- SimonsSearch.Service.Test/SearchEngineWeightCalculatorTests.cs
+++ SimonsSearch.Service.Test/SearchEngineWeightCalculatorTests.cs
@@ -98,5 +98,84 @@ namespace SimonsSearch.Service.Test
             Assert.AreEqual(lck.Name, searchResult.Name);
             Assert.AreEqual(24, searchResult.Weight);
         }
+
+        [Test]
+        public void When_Group_Matched_On_One_Property_Weigth_Should_Be_Calculated()
+        {
+            var group = new Group(Guid.NewGuid(), "Test Group", "This is unit test group");
+
+            var calculator = new SearchEngineWeightCalculator();
+
+            var searchResult = calculator.ToSearchResult(group, "unit");
+
+            Assert.IsNotNull(searchResult);
+            Assert.AreEqual(group.Description, searchResult.Description);
+            Assert.AreEqual(group.Name, searchResult.Name);
+            Assert.AreEqual(5, searchResult.Weight);
+        }
+
+        [Test]
+        public void When_Group_Matched_On_More_Then_One_Property_Weigth_Should_Be_Calculated()
+        {
+            var group = new Group(Guid.NewGuid(), "Test Group", "This is unit test group");
+
+            var calculator = new SearchEngineWeightCalculator();
+
+            var searchResult = calculator.ToSearchResult(group, "test");
+
+            Assert.IsNotNull(searchResult);
+            Assert.AreEqual(group.Description, searchResult.Description);
+            Assert.AreEqual(group.Name, searchResult.Name);
+            Assert.AreEqual(14, searchResult.Weight);
+        }
+
+        [Test]
+        public void When_Media_Matched_On_More_Then_One_Property_Weigth_Should_Be_Calculated()
+        {
+            var media = new Media(Guid.NewGuid(), Guid.NewGuid(), "Media", "Test Owner", "This is unit test media", "T123T");
+
+            var calculator = new SearchEngineWeightCalculator();
+
+            var searchResult = calculator.ToSearchResult(media, "test");
+
+            Assert.IsNotNull(searchResult);
+            Assert.AreEqual(media.Description, searchResult.Description);
+            Assert.AreEqual(string.Empty, searchResult.Name);
+            Assert.AreEqual(16, searchResult.Weight);
+        }
+
+        [Test]
+        public void When_Media_Matched_As_Transient_On_Zero_Properties_Weigth_Should_Be_Calculated()
+        {
+            var group = new Group(Guid.NewGuid(), "Test Group", "This is unit test group");
+            var media = new Media(Guid.NewGuid(), group.Id, "Media", "Test Owner", "This is unit test media", "T123T");
+
+            var calculator = new SearchEngineWeightCalculator();
+
+            var searchResult = calculator.ToTransientSearchResult(media, group, "group");
+
+            Assert.IsNotNull(searchResult);
+            Assert.AreEqual(media.Description, searchResult.Description);
+            Assert.AreEqual(string.Empty, searchResult.Name);
+            Assert.AreEqual(8, searchResult.Weight);
+        }
+
+        [Test]
+        public void When_Media_Matched_As_Transient_On_Own_Properties_Weigth_Should_Be_Calculated()
+        {
+            var group = new Group(Guid.NewGuid(), "Test Group", "This is unit test group");
+            var media = new Media(Guid.NewGuid(), group.Id, "Media", "Test Owner", "This is unit test media", "T123T");
+
+            var calculator = new SearchEngineWeightCalculator();
+
+            var searchResult = calculator.ToTransientSearchResult(media, group, "test");
+
+            Assert.IsNotNull(searchResult);
+            Assert.AreEqual(media.Description, searchResult.Description);
+            Assert.AreEqual(string.Empty, searchResult.Name);
+            Assert.AreEqual(24, searchResult.Weight);
+        }
+
+
     }
 }
